7c43b53fb44129a93d7a312efe68ea538f6ee73e
Will Dietz <w@wdtz.org>
2010-08-21 12:58:50 -0500

    config.sh: support pre/pixi both.
    Refactored out the device-specific flags to support building for both
    pre and pixi.  Unfortunately the pixi doesn't support the ubfx
    instruction (and possibly others) so I'll have to fall back to
    C implementations there or write pixi-specific assembly.

--- src/config.sh
+++ src/config.sh
@@ -1,5 +1,8 @@
 #Note that I am using 2010q1 toolchain here, fwiw
-export CFLAGS='-O3 -mcpu=cortex-a8 -mfpu=neon -ftree-vectorize -mfloat-abi=softfp -ffast-math -DARM_CORE -DFINAL_VERSION'
+export PRE_FLAGS='-mcpu=cortex-a8 -mfpu=neon -mfloat-abi=softfp'
+export PIXI_FLAGS='-mcpu=arm1136jf-s -mfpu=vfp -mfloat-abi=softfp -DPIXI'
+
+export CFLAGS="-O3 $PRE_FLAGS -ftree-vectorize -ffast-math -DARM_CORE -DFINAL_VERSION"
 export CPPFLAGS=$CFLAGS
 export CXXFLAGS=$CFLAGS
 export LIBS='-L/usr/local/lib -L/usr/lib -lGLESv2 -lSDL_ttf -lSDL_image -lpdl -Wl,-rpath,/usr/local/lib'
