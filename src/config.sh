07cbff5d13bf1c20a306c32458769624b1adc5ac
Will Dietz <w@wdtz.org>
2010-08-24 01:40:57 -0500

    update build to use --allow-shlib-undefined linker to prevent linking in undesired libs

--- src/config.sh
+++ src/config.sh
@@ -6,4 +6,5 @@ export CFLAGS="-O3 $PIXI_FLAGS -ftree-vectorize -ffast-math -DFINAL_VERSION"
 export CPPFLAGS=$CFLAGS
 export CXXFLAGS=$CFLAGS
 export LIBS='-L/usr/local/lib -L/usr/lib -lGLESv2 -lSDL_ttf -lSDL_image -lpdl -Wl,-rpath,/usr/local/lib'
+export LDFLAGS='-Wl,--allow-shlib-undefined'
 ./configure --disable-dev --disable-profiling
