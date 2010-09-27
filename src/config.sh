d9b7cac0d98ab186f79fb0a6ac018629eb27fa9e
Will Dietz <w@wdtz.org>
2010-09-27 07:13:06 -0500

    Add rom selector surface cache. Add scrollbar. Add kinetic scrolling.

--- src/config.sh
+++ src/config.sh
@@ -5,5 +5,5 @@ export PIXI_FLAGS='-mcpu=arm1136jf-s -mfpu=vfp -mfloat-abi=softfp -DPIXI'
 export CFLAGS="-O3 $PRE_FLAGS -ftree-vectorize -ffast-math -DARM_CORE -DFINAL_VERSION"
 export CPPFLAGS=$CFLAGS
 export CXXFLAGS=$CFLAGS
-export LIBS='-L/usr/local/lib -L/usr/lib -lGLESv2 -lSDL_ttf -lSDL_image -lpdl -Wl,-rpath,/usr/local/lib'
+export LIBS='-L/usr/local/lib -L/usr/lib -lGLESv2 -lSDL_ttf -lSDL_image -lpdl -Wl,-rpath,/usr/local/lib /usr/lib/libstdc++.a'
 ./configure --disable-dev --disable-profiling
