2fc56ead8356e62f884b688767e6846c5f747fbc
Simon Ruggier <Simon80@gmail.com>
2011-04-28 18:01:58 -0400

    Remove erroneous CPPFLAGS setting in src/config.sh
    CPPFLAGS is for the C Preprocessor, not g++, so it doesn't make sense to
    pass CFLAGS there.

--- src/config.sh
+++ src/config.sh
@@ -5,7 +5,6 @@ export PRE_FLAGS='-mcpu=cortex-a8 -mfpu=neon -mfloat-abi=softfp'
 export PIXI_FLAGS='-mcpu=arm1136jf-s -mfpu=vfp -mfloat-abi=softfp -DPIXI'
 
 export CFLAGS="-O3 $PRE_FLAGS -ftree-vectorize -ffast-math -DARM_CORE -DFINAL_VERSION"
-export CPPFLAGS=$CFLAGS
 export CXXFLAGS=$CFLAGS
 export LIBS='-L/usr/local/lib -L/usr/lib -lGLESv2 -lSDL_ttf -lSDL_image -lpdl -Wl,-rpath,/usr/local/lib /usr/lib/libstdc++.a'
 ./configure --disable-dev --disable-profiling --with-sdl-prefix=/usr/local
