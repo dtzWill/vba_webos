a7f062d1145b992ecf760926417505fd727ddcfe
Simon Ruggier <Simon80@gmail.com>
2011-04-29 16:51:13 -0400

    Split off the generic parts of config.sh into autogen.sh

--- src/config.sh
+++ src/config.sh
@@ -1,10 +1,3 @@
-autoreconf --install --symlink
+export LIBS='-L/usr/local/lib -L/usr/lib -Wl,-rpath,/usr/local/lib /usr/lib/libstdc++.a'
 
-#Note that I am using 2010q1 toolchain here, fwiw
-export PRE_FLAGS='-mcpu=cortex-a8 -mfpu=neon -mfloat-abi=softfp'
-export PIXI_FLAGS='-mcpu=arm1136jf-s -mfpu=vfp -mfloat-abi=softfp -DPIXI'
-
-export CFLAGS="-O3 $PRE_FLAGS -ftree-vectorize -ffast-math -DARM_CORE -DFINAL_VERSION"
-export CXXFLAGS=$CFLAGS
-export LIBS='-L/usr/local/lib -L/usr/lib -lGLESv2 -lSDL_ttf -lSDL_image -lpdl -Wl,-rpath,/usr/local/lib /usr/lib/libstdc++.a'
-./configure --disable-dev --disable-profiling --with-sdl-prefix=/usr/local
+./autogen.sh --with-sdl-prefix=/usr/local
