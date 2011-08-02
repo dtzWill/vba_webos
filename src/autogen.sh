d5cd617f23b21092be20bbdc8eb945abf6f71f9e
Will Dietz <w@wdtz.org>
2011-08-01 22:32:49 -0500

    Add -DPIXI -UPRE to make the SDL includes happy.
    I don't like this commit, I'm unsure why PRE is being defined in the
    first place.  Hrmmm.

--- src/autogen.sh
+++ src/autogen.sh
@@ -2,7 +2,7 @@ autoreconf --install --symlink
 
 #Note that I am using 2010q1 toolchain here, fwiw
 export PRE_FLAGS='-mcpu=cortex-a8 -mfpu=neon -mfloat-abi=softfp -DARM_CORE -DPALM_PRE'
-export PIXI_FLAGS='-mcpu=arm1136jf-s -mfpu=vfp -mfloat-abi=softfp -DPALM_PIXI'
+export PIXI_FLAGS='-mcpu=arm1136jf-s -mfpu=vfp -mfloat-abi=softfp -DPALM_PIXI -DPIXI -UPRE'
 
 export EXTRA_CFLAGS="-O3 -ggdb $PIXI_FLAGS -ftree-vectorize -ffast-math -DFINAL_VERSION"
 export CFLAGS="$EXTRA_CFLAGS $CFLAGS"
